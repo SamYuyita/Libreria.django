183b16424fc9f8f423b49e3090d105bea932fd27
SamYuyita <sammzamponi@gmail.com>
2023-10-12 20:04:52 -0300

    Parte 6: Lista de libros

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
  
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """
@@ -20,3 +20,21 @@ def index(request):
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
 
+class BookListView(generic.ListView):
+    model = Book
+    
+    # su propio nombre para la lista como variable de plantilla
+    context_object_name = 'my_book_list'   
+    
+    # Consigue 5 libros que contengan el título de guerra.
+    #def get_queryset(self):
+    #   return Book.objects.filter(title__icontains='war')[:5]
+    # Especifique su propio nombre/ubicación de plantilla
+    #template_name = 'books/my_arbitrary_template_name_list.html'  
+    
+    #def get_context_data(self, **kwargs):
+    #    # Llame primero a la implementación base para obtener un contexto.
+    #    context = super(BookListView, self).get_context_data(**kwargs)
+    #    # Obtenga el blog del id y agréguelo al contexto.
+    #    context['some_data'] = 'Estos son solo algunos datos'
+    #    return context
